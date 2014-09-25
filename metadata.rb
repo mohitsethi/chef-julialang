1bb1c7ab06446530f39b4b44c7fc31110a18af9c
mohitsethi <mohit@sethis.in>
2014-09-25 06:43:51 +0000

    Bumped to v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mohit@sethis.in'
 license          'Apache 2.0'
 description      'Installs/Configures JuliaLang'
 long_description 'Installs/Configuresi JuliaLang'
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'debian'
 supports 'ubuntu'
